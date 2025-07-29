4acf9e7b089f52e91d75ab1ada75c232fc9b5fbb
Temidayo Emmanuel Fagbemi <82624396+zordtech@users.noreply.github.com>
2025-07-29 17:58:49 +0100

    Add files via upload

---
+++ week 2 assignment/app.py
@@ -0,0 +1,12 @@
+#Week 2 PLP assignment
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop(-1)
+my_list.sort()
+print(my_list.index(30))
+print(my_list)
